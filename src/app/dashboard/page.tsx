577934827ba0a18e10a3befc77e15c60a13581c4
4epiz <4epiz.dev@gmail.com>
2025-02-17 18:19:42 +0000

    shadcn

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Dashboard() {
+    <div className="min-h-screen flex bg-zinc-900">
+        <h1>hello</h1>
+    </div>
+}
