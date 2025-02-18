8ed05498c1598838a981627361322af3c9637a66
4epiz <4epiz.dev@gmail.com>
2025-02-18 11:48:40 +0000

    Login ui enchancement

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,8 +2,12 @@
 import Image from "next/image";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link"
 
 import { LoaderCircle } from "lucide-react";
+import { Label } from "@/components/shadcn/label";
+import { Checkbox } from "@/components/shadcn/checkbox";
+import { Input } from "@/components/shadcn/input";
 import { Button } from "@/components/shadcn/button";
 export default function Login() {
   const router = useRouter();
@@ -54,131 +58,127 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen flex dark bg-background">
-      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24 border-r border-neutral-950/20">
-        <div className="mx-auto w-full max-w-sm lg:w-96">
-          <div>
-            <Image
-              className="h-12 w-auto rounded-xl"
-              src="/logo.png"
-              alt="Airlink Logo"
-              width={48}
-              height={48}
-            />
-            <h2 className="mt-6 text-3xl font-medium text-neutral-800 dark:text-white">
-              Sign in to Airlink
-            </h2>
-            <p className="mt-2 text-sm font-normal text-neutral-500">
-              Please enter your account credentials below.
-            </p>
-          </div>
-
-          <div className="mt-8">
-            <form className="space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="username"
-                  className="block text-sm font-medium text-neutral-400"
-                >
-                  Username or Email
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    id="username"
-                    placeholder="example"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    className="w-full px-3 py-2 bg-white/10 border border-neutral-800/10 rounded-xl placeholder-neutral-500 focus:outline-none focus:ring-neutral-500 focus:border-neutral-500 text-black dark:text-white sm:text-sm"
-                    required
-                  />
-                </div>
+    <section className="flex items-center justify-center dark bg-background text-white overflow-y-hidden h-screen">
+      <div className="">
+        <div className="grid lg:grid-cols-2">
+          <div className="relative overflow-hidden py-10">
+            <div className="mx-auto my-auto flex h-full w-full max-w-md flex-col  gap-4 p-6">
+              <div className="mb-6 flex flex-col items-center text-center">
+                <Image src="/logo.png" alt="logo" width={40} height={40} className="mb-7 h-10 w-auto" />
+                <p className="mb-2 text-2xl font-bold">Welcome to AirLink</p>
+                <p className="text-muted-foreground">Please enter your credentials to continue.</p>
               </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-neutral-400"
-                >
-                  Password
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="********"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="w-full px-3 py-2 bg-white/10 border border-neutral-800/10 rounded-xl placeholder-neutral-500 focus:outline-none focus:ring-neutral-500 focus:border-neutral-500 text-black dark:text-white sm:text-sm"
-                    required
-                  />
-                </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    type="checkbox"
-                    id="remember-me"
-                    className="h-4 w-4 text-neutral-600 focus:ring-neutral-500 border-neutral-800/10 bg-neutral-800/20 dark:border-white/10 dark:bg-white/5 rounded"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-neutral-400"
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="/auth/reset-password"
-                    className="font-normal text-neutral-500 hover:text-white transition"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
-              <div>
-                <Button
-                  type="submit"
-                  onClick={handleClick}
-                  disabled={isLoading}
-                  data-loading={isLoading}
-                  className="group relative disabled:opacity-100 w-full flex justify-center py-2 px-4 border border-neutral-800/20 rounded-xl shadow-sm text-sm font-medium text-neutral-200 dark:text-neutral-800 bg-neutral-950 dark:bg-white hover:bg-neutral-900 dark:hover:bg-neutral-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-500"
-                >
-                  <span className="group-data-[loading=true]:text-transparent">Sign in</span>
-                  {isLoading && (
-                    <div className="absolute inset-0 flex items-center justify-center">
-                      <LoaderCircle className="animate-spin" size={16} strokeWidth={2} aria-hidden="true" />
+              <div className="w-full rounded-md bg-background">
+                <div>
+                  <div className="grid gap-4">
+                    <div className="grid w-full items-center gap-1.5">
+                      <Label htmlFor="username">Email</Label>
+                      <Input
+                        type="text"
+                        id="username"
+                        placeholder="example"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        className="text-base md:text-sm"
+                        required
+                      />
                     </div>
-                  )}
-                </Button>
+                    <div>
+                      <div className="grid w-full items-center gap-1.5">
+                        <Label htmlFor="password">Password</Label>
+                        <Input
+                          type="password"
+                          id="password"
+                          placeholder="********"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          className="text-base md:text-sm"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="flex justify-between">
+                      <div className="flex items-center space-x-2">
+                        <Checkbox id="remember" className="border-muted-foreground" />
+                        <Label htmlFor="remember">Remember me</Label>
+                      </div>
+                      <Link href="#" className="text-sm font-medium text-primary">
+                        Forgot password
+                      </Link>
+                    </div>
+                    <Button
+                    type="submit"
+                      onClick={handleClick}
+                      disabled={isLoading}
+                      data-loading={isLoading}
+                      className="group relative disabled:opacity-100"
+                    >
+                      <span className="group-data-[loading=true]:text-transparent">Sign in</span>
+                      {isLoading && (
+                        <div className="absolute inset-0 flex items-center justify-center">
+                          <LoaderCircle className="animate-spin" size={16} strokeWidth={2} aria-hidden="true" />
+                        </div>
+                      )}
+                    </Button>
+                    <Button variant="outline" className="w-full">
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeWidth="0"
+                        version="1.1"
+                        x="0px"
+                        y="0px"
+                        viewBox="0 0 48 48"
+                        enableBackground="new 0 0 48 48"
+                        className="mr-2 size-5"
+                        height="1em"
+                        width="1em"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill="#FFC107"
+                          d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
+                          c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
+                          c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                        />
+                        <path
+                          fill="#FF3D00"
+                          d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
+                          C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                        />
+                        <path
+                          fill="#4CAF50"
+                          d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
+                          c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                        />
+                        <path
+                          fill="#1976D2"
+                          d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
+                          c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                        />
+                      </svg>
+                      Sign up with Google
+                    </Button>
+                  </div>
+                </div>
               </div>
-
-              <div className="mt-6 text-sm text-neutral-500">
-                Need an account?{' '}
-                <a
-                  href="/register"
-                  className="font-normal text-neutral-500 hover:text-white transition"
-                >
-                  Register
-                </a>
+              <div className="mx-auto mt-3 flex justify-center gap-1 text-sm text-muted-foreground">
+                <p>Don&apos;t have an account?</p>
+                <Link href="#" className="font-medium text-primary">
+                  Sign up
+                </Link>
               </div>
-            </form>
+            </div>
           </div>
+          <Image
+            src="/login-bg.jpeg"
+            alt="placeholder"
+            width={700}
+            height={800}
+            className="hidden h-full max-h-screen w-md object-cover lg:block rounded-xl"
+          />
         </div>
       </div>
-
-      {/* Background iamge */}
-      <div className="hidden lg:block relative w-0 flex-1">
-        <img
-          className="absolute inset-0 h-full w-full object-cover"
-          src="https://i.imgur.com/j9BodUY.jpeg"
-          alt="Background"
-        />
-      </div>
-    </div>
+    </section>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,19 @@
-"use client";
-import Image from "next/image";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-
+import { Button } from "@/components/shadcn/button";
+import { Card } from "@/components/shadcn/card";
+import { LayoutDashboard, Settings, Database, Server, Users, Layers, PlugZap } from "lucide-react";
+import Sidebar from "@/components/airlink/sidebar";
+import TopNav from "@/components/airlink/topnav";
 export default function Dashboard() {
-    <div className="min-h-screen flex bg-zinc-900">
-        <h1>hello</h1>
+  return (
+    <div className={`flex h-screen dark bg-background`}>
+    <Sidebar />
+    <div className="w-full flex flex-1 flex-col">
+      <header className="h-16 border-b border-gray-200 dark:border-[#1F1F23]">
+        <TopNav />
+      </header>
+      {/* <main className="flex-1 overflow-auto p-6 bg-white dark:bg-[#0F0F12]">{children}</main> */}
     </div>
+  </div>
+  );
 }
+
